8cd8d58c73d0f624c6737e8b99f5ba26af214332
Adam Kaliński <adamkalinski@gmail.com>
2018-01-10 21:27:34 +0100

    Init

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+"""Implements FizzBuzz algorithm
+
+https://en.wikipedia.org/wiki/Fizz_buzz
+"""
+
+
+def fizzbuzz(i):
+    """FizzBuzz algorithm"""
+    return None
---
+++ test_fizzbuzz.py
@@ -0,0 +1,17 @@
+import fizzbuzz
+
+
+def test_fizzbuzz_0():
+    assert fizzbuzz.fizzbuzz(0) == "FizzBuzz"
+
+
+def test_fizzbuzz_3():
+    assert fizzbuzz.fizzbuzz(3) == "Fizz"
+
+
+def test_fizzbuzz_5():
+    assert fizzbuzz.fizzbuzz(5) == "Buzz"
+
+
+def test_fizzbuzz_4():
+    assert fizzbuzz.fizzbuzz(4) == "4"
